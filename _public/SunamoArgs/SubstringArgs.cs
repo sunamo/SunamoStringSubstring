99546c6272f19d939b13cce0a8cf248abf6d939f
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 18:36:50 +0200

    All buildable, before removing dead code

---
+++ _public/SunamoArgs/SubstringArgs.cs
@@ -0,0 +1,15 @@
+namespace SunamoStringSubstring;
+
+
+
+
+
+public class SubstringArgs
+{
+    
+    
+    
+    public bool returnInputIfInputIsShorterThanA3 = false;
+    public bool returnInputIfIndexFromIsLessThanIndexTo = false;
+    public static SubstringArgs Instance = new SubstringArgs();
+}
