403c2e8cfcffd7b4ffaa3590305ccadcc241df9b
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:37:19 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoStringSubstring/_sunamo/SunamoExceptions/ThrowEx.cs
+++ SunamoStringSubstring/_sunamo/SunamoExceptions/ThrowEx.cs
@@ -1,6 +1,7 @@
+namespace SunamoStringSubstring._sunamo.SunamoExceptions;
+
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoStringSubstring._sunamo.SunamoExceptions;
 internal partial class ThrowEx
 {
     internal static bool ArgumentOutOfRangeException(string argName, string message = "")
